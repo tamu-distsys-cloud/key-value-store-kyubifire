91c8f681a95b9ea509193b8d6259136989f3524d
Nilo Lisboa <nilolisboa@Nilos-MacBook-Pro-2.local>
2025-06-18 16:30:15 -0500

    passing part 2

--- client.py
+++ client.py
@@ -14,6 +14,10 @@ class Clerk:
         self.cfg = cfg
         self.request_id = 0
         self.client_id = nrand()
+        #self.nshards = cfg.nshards
+
+    # def get_shard(self, key: str) -> int:
+    #     return int(key) % self.nshards
 
     # Fetch the current value for a key.
     # Returns "" if the key does not exist.
@@ -27,13 +31,19 @@ class Clerk:
     # must match the declared types of the RPC handler function's
     # arguments in server.py.
     def get(self, key: str) -> str:
-        # You will have to modify this function.
-        key = GetArgs(key)
-        for server in self.servers:
-            reply = server.call("KVServer.Get", key)
-            if reply is not None:
-                return reply.value
-        return ""
+        self.request_id += 1
+        args = GetArgs(key, self.client_id, self.request_id)
+        # shard = self.get_shard(key)
+        while True:
+            for server in self.servers:
+                try:
+                    reply = server.call("KVServer.Get", args)
+                    if reply is not None:
+                        return reply.value
+                    else:
+                        return ""
+                except TimeoutError:
+                    continue
 
     # Shared by Put and Append.
     #
@@ -45,16 +55,18 @@ class Clerk:
     # must match the declared types of the RPC handler function's
     # arguments in server.py.
     def put_append(self, key: str, value: str, op: str) -> str:
-        # with self.mu:
-        #     self.request_id += 1
-        #     req = self.request_id
-        args = PutAppendArgs(key, value)
-
-        for server in self.servers:
-            reply = server.call("KVServer."+op, args)
-            if reply is not None:
-                return reply if op == "Append" else ""
-        return ""
+        self.request_id += 1
+        args = PutAppendArgs(key, value, self.client_id, self.request_id)
+        while True:
+            for server in self.servers:
+                try:
+                    reply = server.call("KVServer." + op, args)
+                    if op == "Append":
+                        return reply
+                    else:
+                        return ""
+                except TimeoutError:
+                    continue
 
     def put(self, key: str, value: str):
         self.put_append(key, value, "Put")
--- server.py
+++ server.py
@@ -17,6 +17,7 @@ class PutAppendArgs:
         self.value = value
         self.client_id = client_id
         self.req_id = req_id
+        # rename this is req_counter
 
 class PutAppendReply:
     # Add definitions here if needed
@@ -40,7 +41,7 @@ class KVServer:
         self.mu = threading.Lock()
         self.cfg = cfg
         self.kv = dict()
-        self.client_req = dict()
+        self.client_req = {}
 
     def Get(self, args: GetArgs):
         reply = GetReply(None)
@@ -49,11 +50,29 @@ class KVServer:
         return reply
 
     def Put(self, args: PutAppendArgs):
+        # with self.mu:
+        #     self.kv[args.key] = args.value
+        # client_id -> (req_id, success/fail)
         with self.mu:
+            last_req = self.client_req.get(args.client_id)
+            print(last_req)
+            if last_req is not None and last_req[0] >= args.req_id:
+                return  # Duplicate or older request, ignore
+
             self.kv[args.key] = args.value
+            self.client_req[args.client_id] = (args.req_id, None)
 
     def Append(self, args: PutAppendArgs):
+        # with self.mu:
+        #     previous = self.kv[args.key]
+        #     self.kv[args.key] += args.value
+        #     return previous
         with self.mu:
-            previous = self.kv[args.key]
-            self.kv[args.key] += args.value
-            return previous
+            last_req = self.client_req.get(args.client_id)
+            if last_req is not None and last_req[0] >= args.req_id:
+                return last_req[1]  # Return previous reply
+
+            prev = self.kv.get(args.key, "")
+            self.kv[args.key] = prev + args.value
+            self.client_req[args.client_id] = (args.req_id, prev)
+            return prev
